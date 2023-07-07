16198e17d0f57cc618d4221b4b9955baf5ae6536
santivediap <santivediap@gmail.com>
2023-07-07 21:22:36 +0200

    Create pokemons list state and link base app components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from './Search'
+import ListaPokemon from './ListaPokemon'
 
 const Main = () => {
-  return <div>Main</div>;
+
+  const [ pokemons, setPokemons ] = useState([])
+
+  return <main>
+    <Search pokemons={pokemons} setPokemons={setPokemons} />
+    {pokemons.length ? <ListaPokemon pokemons={pokemons}/> : <p className="hide"></p>}
+  </main>;
 };
 
 export default Main;
