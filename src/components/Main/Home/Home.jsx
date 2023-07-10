5b0b0569013efe68adeba44c548e0a5416b75e66
santivediap <santivediap@gmail.com>
2023-07-10 23:51:55 +0200

    Fix table types responsive issue

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
 
   return <>
     <img className="app-logo" src="../src/assets/PokeAPP-logo.png" alt="" />
+    <h2 className="types">Pokemon types table</h2>
     <img className="table-types" src="https://e1.pxfuel.com/desktop-wallpaper/887/306/desktop-wallpaper-pokemon-type-chart-by-xx-bannanabread-xx-pokemon-type.jpg" alt="table-types-pokemon" />
   </>
 };
