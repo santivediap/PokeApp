a17eabce03a06da213af2acf1b2b0cab424e9277
santivediap <santivediap@gmail.com>
2023-07-10 23:46:00 +0200

    Add pokemon details route

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Home from './Home'
 import SearchPokemon from "./SearchPokemon/SearchPokemon";
+import Details from "./Details/Details";
 import { Routes, Route } from 'react-router-dom';
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/search" element={<SearchPokemon/>} />
+      <Route path="/pokemon/:id" element={<Details/>} />
     </Routes>
   </main>;
 };
