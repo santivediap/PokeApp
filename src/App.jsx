dbadc435fac6940984d6e80926bf5dd038023f05
santivediap <santivediap@gmail.com>
2023-07-10 13:09:37 +0200

    Create global pokemonsList state and add BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 // import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import { BrowserRouter } from 'react-router-dom';
+import PokemonsContext from './context/pokemonsContext'
 
 function App() {
-  // const [count, setCount] = useState(0)
+
+  const [pokemonsList, setPokemonsList] = useState([]);
+
+  const pokemonsData = {
+    pokemonsList,
+    setPokemonsList
+  }
 
   return (
     <>
-    <Header />
-    <Main />
-    <Footer />
+      <PokemonsContext.Provider value={pokemonsData}>
+        <BrowserRouter>
+          <Header />
+          <Main />
+        </BrowserRouter>
+        <Footer />
+      </PokemonsContext.Provider>
     </>
   )
 }
