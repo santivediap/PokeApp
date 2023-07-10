1dff2f1148c4bce4acde13043c1f0c4d0c313278
santivediap <santivediap@gmail.com>
2023-07-10 13:06:31 +0200

    Use new pokemonsList context

--- src/components/Main/ListaPokemon/ListaPokemon.jsx
+++ src/components/Main/ListaPokemon/ListaPokemon.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import Card from './Card'
+import PokemonsContext from "../../../context/pokemonsContext";
 
-const ListaPokemon = ({pokemons}) => {
+const ListaPokemon = () => {
+
+  const { pokemonsList, setPokemonsList } = useContext(PokemonsContext)
 
   return <section className="search-container">
     <article className="search-results-header">
@@ -10,7 +13,7 @@ const ListaPokemon = ({pokemons}) => {
     </article>
     
     <article className="search-results-data">
-      {pokemons.length?pokemons.map((pokemon, i) => <Card key={i} type={pokemon.type} name={pokemon.name} id={pokemon.id} image={pokemon.image} />):<p className="hide"></p>}
+      {pokemonsList.length?pokemonsList.map((pokemon, i) => <Card key={i} type={pokemon.type} name={pokemon.name} id={pokemon.id} image={pokemon.image} />):<p className="hide"></p>}
     </article>
   </section>;
 };
