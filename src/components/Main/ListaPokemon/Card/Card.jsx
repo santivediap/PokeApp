4f2efb0f59eaa67e21ebc2ee9884137bba681fa4
santivediap <santivediap@gmail.com>
2023-07-10 13:04:22 +0200

    Change pokemons name from h2 to links with ids

--- src/components/Main/ListaPokemon/Card/Card.jsx
+++ src/components/Main/ListaPokemon/Card/Card.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-// import { dark } from '../../../../assets/dark.png'
+import { Link } from "react-router-dom";
 
 const Card = ({ name, id, image, type }) => {
+
+  let fixedId = id.slice(1, id.length);
+  fixedId < 10 ? fixedId = id.slice(3, id.length) : fixedId < 100 ? fixedId = id.slice(2, id.length) : "???";
+  
   return <div className={`${type}-pokemon-container`}>
     <div className="pokedex-number-container">
       <img src={`../src/assets/${type}.png`} alt="type-img" />
@@ -9,9 +13,8 @@ const Card = ({ name, id, image, type }) => {
     </div>
 
     <img src={image} alt={`${name}'s-img`} />
-
     <div className="pokemon-name-container">
-      <h2>{name}</h2>
+      <Link to={`/pokemon/${fixedId}`}>{name}</Link>
     </div>
   </div>;
 };
